6a8034a0752367067689c603c76bab457358c226
Michael Belardo <mbelardo1@gmail.com>
2012-12-04 21:38:31 -0500

    framing work

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ var http = require('http');
 /**/// Returns
 /**/// return     - the_return_value
 var Scrobbler = function(api_key, api_secret, username, password) {
-  this.api_key    = api_key;
-  this.api_secret = api_secret;
+  this.apiKey     = api_key;
+  this.apiSecret  = api_secret;
   this.username   = username;
   this.password   = password;
+  this.sessionKey = null;
+  this.apiSig     = null;
 };
 /**/// Public: Scrobble
 /**///
@@ -25,7 +27,87 @@ var Scrobbler = function(api_key, api_secret, username, password) {
 /**/// Returns
 /**/// return - the_return_value
 Scrobbler.prototype.Scrobble = function(song) {
-
+  // TODO
 };
+/**/// Public: Now Playing
+/**///
+/**/// Args
+/**/// song - the_arg_value
+/**///
+/**/// Returns
+/**/// return - the_return_value
+Scrobbler.prototype.NowPlaying = function(song) {
+  // TODO
+};
+/**/// Public: Create
+/**///
+/**/// Args
+/**/// arg1 - the_arg_value
+/**/// arg2 - the_arg_value
+/**///
+/**/// Returns
+/**/// return - the_return_value
+Scrobbler.prototype.MakeSession = function() {
+  // TODO
+};
+/**/// Public: Call GET API
+/**///
+/**/// Args
+/**/// path - the_arg_value
+/**///
+/**/// Returns
+/**/// return - the_return_value
+function getAPI(path) {
+  var callResponse = ''
+    , callValues   =  {
+                        host: 'ws.audioscrobbler.com',
+                        port: 80,
+                        path: path
+                      };
+  http.get(callValues, function(response) {
+    response.on('data', function(chunk) {
+      callResponse += chunk
+    });
+    response.on('end', function() {
+      return JSON.parse(callResponse);
+    });
+  }).on('error', function(err) {
+    // TODO
+  });
+}
+/**/// Public: Call POST API
+/**///
+/**/// Args
+/**/// path - the_arg_value
+/**///
+/**/// Returns
+/**/// return - the_return_value
+function postAPI(path) {
+  var callValues =  {
+                      host: 'ws.audioscrobbler.com',
+                      port: 80,
+                      method: 'POST',
+                      path: path
+                    };
+  http.request(callValues, function(request) {
+    request.setEncoding('utf8');
+    request.on('data', function(chunk) {
+      // TODO
+      return true;
+    });
+  }).on('error', function(err) {
+    // TODO
+  });
+}
+/**/// Public: Make MD5 hashes
+/**///
+/**/// Args
+/**/// input - the_arg_value
+/**///
+/**/// Returns
+/**/// return - the_return_value
+function makeHash(input) {
+  return crypto.createHash('md5').update(string, 'utf8').digest("hex");
+}
 
 module.exports = Scrobbler;
