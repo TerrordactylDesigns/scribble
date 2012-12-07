66ce461acb8ed8243ef071564b0421ca2ed1e0f4
Michael Belardo <mbelardo1@gmail.com>
2012-12-06 21:18:41 -0500

    it works....

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 #Scribble
 ###Simple Last.fm scrobbler for node.js
+
+#####Scribble down the song
+
+###PreReqs
+
+Get a last.fm API account and save the api key, api secret, and your username and password.
+
+###Install
+    npm install scribble
+
+###Use it
+
+    var scribble = require('scribble');
+    var Scrobbler = new scribble('your_api_key','your_api_secret','your_lastfm_username','your_lastfm_password');
+    var song = {
+      artist: 'Slayer',
+      track: 'Disciple'
+    };
+    Scrobbler.Scrobble(song);
+
+##F * A * Q
+
+* Q: This isnt working
+* A: Sorry....
+***
+* Q: Why doesn't it do now playing?
+* A: It will.
--- index.js
+++ index.js
@@ -1,64 +1,104 @@
 /**/// GLOBALS
-var http = require('http');
-
-/**/// Public: Scrobbler
+var http        = require('http')
+  , crypto      = require('crypto')
+  , querystring = require('querystring');
+/**/// Public: Scribble
 /**///
 /**/// Args
-/**/// api_key    - the_arg_value
-/**/// api_secret - the_arg_value
-/**/// username   - the_arg_value
-/**/// password   - the_arg_value
+/**/// api_key    - Last.fm API account key
+/**/// api_secret - Last.fm API secret
+/**/// username   - Last.fm account
+/**/// password   - Last.fm password
 /**///
 /**/// Returns
-/**/// return     - the_return_value
-var Scrobbler = function(api_key, api_secret, username, password) {
+/**/// return     - A scribble
+var Scribble = function(api_key, api_secret, username, password) {
   this.apiKey     = api_key;
   this.apiSecret  = api_secret;
   this.username   = username;
   this.password   = password;
   this.sessionKey = null;
-  this.apiSig     = null;
 };
+/**/// Public: Post Scrobble
+/**///
+/**/// Args
+/**/// song - song object. artist, track keys
+/**/// sk   - authenticated session key
+/**///
+/**/// Returns
+/**/// return - xml response from scrobble
+Scribble.prototype.postScrobble = function(song, sk) {
+  if (sk && this.sessionKey == null) {
+    this.sessionKey = sk;
+  }
+  var path = ''
+    , now = new Date().getTime()
+    , timestamp = Math.floor(now /1000)
+    , token       = makeHash(this.username + makeHash(this.password))
+    , apiSig     = makeHash('api_key' + this.apiKey + 'artist' + song.artist + 'methodtrack.scrobblesk' + this.sessionKey + 'timestamp' + timestamp + 'track' + song.track + this.apiSecret)
+    , post_data = querystring.stringify({
+        method: 'track.scrobble',
+        api_key: this.apiKey,
+        sk: this.sessionKey,
+        api_sig: apiSig,
+        timestamp: timestamp,
+        artist: song.artist,
+        track: song.track
+      })
+    , post_options = {
+        host: 'ws.audioscrobbler.com',
+        path: '/2.0/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': post_data.length
+        }
+      }
+    , sendScrobble = http.request(post_options, function(request) {
+        var reqReturn = '';
+        request.setEncoding('utf8');
+        request.on('data', function(chunk) {
+          reqReturn += chunk;
+        });
+        request.on('end', function() {
+          console.log('[SCROBBLE RESPONSE] : '+reqReturn);
+        });
+      }).on('error', function(err) {
+        // TODO
+      });
+  sendScrobble.write(post_data);
+  sendScrobble.end();
+}
 /**/// Public: Scrobble
 /**///
 /**/// Args
-/**/// song - the_arg_value
-/**///
-/**/// Returns
-/**/// return - the_return_value
-Scrobbler.prototype.Scrobble = function(song) {
-  // TODO
+/**/// song - song object. artist, track keys
+Scribble.prototype.Scrobble = function(song) {
+  if (this.sessionKey == null) {
+    var self = this;
+    this.MakeSession(function(sk) {
+      self.postScrobble(song,sk);
+    });
+  } else {
+    this.postScrobble(song)
+  }
 };
-/**/// Public: Now Playing
+/**/// Public: Make session key
 /**///
 /**/// Args
-/**/// song - the_arg_value
+/**/// callback - optional callback function
 /**///
 /**/// Returns
-/**/// return - the_return_value
-Scrobbler.prototype.NowPlaying = function(song) {
-  // TODO
-};
-/**/// Public: Create
-/**///
-/**/// Args
-/**/// arg1 - the_arg_value
-/**/// arg2 - the_arg_value
-/**///
-/**/// Returns
-/**/// return - the_return_value
-Scrobbler.prototype.MakeSession = function() {
-  // TODO
-};
-/**/// Public: Call GET API
-/**///
-/**/// Args
-/**/// path - the_arg_value
-/**///
-/**/// Returns
-/**/// return - the_return_value
-function getAPI(path) {
-  var callResponse = ''
+/**/// return - a session key and optional callback
+Scribble.prototype.MakeSession = function(callback) {
+  var token   = makeHash(this.username + makeHash(this.password))
+    , apiSig  = makeHash('api_key' + this.apiKey + 'authToken' + token + 'methodauth.getMobileSessionusername' + this.username + this.apiSecret)
+    , path    = '/2.0/?method=auth.getMobileSession&' +
+                'username=' + this.username + '&' +
+                'authToken=' + token + '&' +
+                'api_key=' + this.apiKey + '&' +
+                'api_sig=' + apiSig + '&format=json'
+    , callResponse = ''
     , callValues   =  {
                         host: 'ws.audioscrobbler.com',
                         port: 80,
@@ -69,45 +109,32 @@ function getAPI(path) {
       callResponse += chunk
     });
     response.on('end', function() {
-      return JSON.parse(callResponse);
+      var test = JSON.parse(callResponse);
+      this.sessionKey = test.session.key;
+      if (typeof(callback) == 'function') {
+        callback(test.session.key);
+      }
     });
   }).on('error', function(err) {
     // TODO
   });
-}
-/**/// Public: Call POST API
+};
+/**/// Public: Now Playing
 /**///
 /**/// Args
-/**/// path - the_arg_value
-/**///
-/**/// Returns
-/**/// return - the_return_value
-function postAPI(path) {
-  var callValues =  {
-                      host: 'ws.audioscrobbler.com',
-                      port: 80,
-                      method: 'POST',
-                      path: path
-                    };
-  http.request(callValues, function(request) {
-    request.setEncoding('utf8');
-    request.on('data', function(chunk) {
-      // TODO
-      return true;
-    });
-  }).on('error', function(err) {
-    // TODO
-  });
-}
+/**/// song - song object. artist, track keys
+Scribble.prototype.NowPlaying = function(song) {
+  // TODO
+};
 /**/// Public: Make MD5 hashes
 /**///
 /**/// Args
-/**/// input - the_arg_value
+/**/// input - string input to hash
 /**///
 /**/// Returns
-/**/// return - the_return_value
+/**/// return - md5 hash of the input string
 function makeHash(input) {
-  return crypto.createHash('md5').update(string, 'utf8').digest("hex");
+  return crypto.createHash('md5').update(input, 'utf8').digest("hex");
 }
 
-module.exports = Scrobbler;
+module.exports = Scribble;
