a2b37f585c5382863319115db36201399477ed43
Andreas Niedermair <andreas@niedermair.name>
2016-01-20 14:38:46 +0100

    added magic underscore album :)

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ function postLove(self, song, sk, callback) {
   if (sk && self.sessionKey == null) {
     self.sessionKey = sk
   }
-  var apiSig    = makeHash('album' + song.album + 'api_key' + self.apiKey + 'artist' + song.artist + 'methodtrack.lovesk' + self.sessionKey + 'track' + song.track + self.apiSecret)
+  var apiSig    = makeHash('album' + (song.album || '_') + 'api_key' + self.apiKey + 'artist' + song.artist + 'methodtrack.lovesk' + self.sessionKey + 'track' + song.track + self.apiSecret)
     , post_data = querystring.stringify({
         method: 'track.love',
         api_key: self.apiKey,
@@ -235,7 +235,7 @@ function postLove(self, song, sk, callback) {
         api_sig: apiSig,
         artist: song.artist,
         track: song.track,
-        album: song.album
+        album: song.album || '_'
       })
   sendPost(post_data, callback)
 }
@@ -254,12 +254,12 @@ function postNowPlaying(self, song, sk, callback) {
     self.sessionKey = sk
   }
   var dur       = (song.duration) ? 'duration' + song.duration : ''
-    , apiSig    = makeHash('album' + song.album + 'api_key' + self.apiKey + 'artist' + song.artist + dur + 'methodtrack.updateNowPlayingsk' + self.sessionKey + 'track' + song.track + self.apiSecret)
+    , apiSig    = makeHash('album' + (song.album || '_') + 'api_key' + self.apiKey + 'artist' + song.artist + dur + 'methodtrack.updateNowPlayingsk' + self.sessionKey + 'track' + song.track + self.apiSecret)
     , post_data = querystring.stringify({
         method: 'track.updateNowPlaying',
         artist: song.artist,
         track: song.track,
-        album: song.album,
+        album: song.album || '_',
         duration: song.duration,
         api_key: self.apiKey,
         api_sig: apiSig,
@@ -283,7 +283,7 @@ function postScrobble(self, song, sk, callback) {
   }
   var now       = new Date().getTime()
     , timestamp = Math.floor(now /1000)
-    , apiSig    = makeHash('album' + song.album + 'api_key' + self.apiKey + 'artist' + song.artist + 'methodtrack.scrobblesk' + self.sessionKey + 'timestamp' + timestamp + 'track' + song.track + self.apiSecret)
+    , apiSig    = makeHash('album' + (song.album || '_') + 'api_key' + self.apiKey + 'artist' + song.artist + 'methodtrack.scrobblesk' + self.sessionKey + 'timestamp' + timestamp + 'track' + song.track + self.apiSecret)
     , post_data = querystring.stringify({
         method: 'track.scrobble',
         api_key: self.apiKey,
@@ -292,7 +292,7 @@ function postScrobble(self, song, sk, callback) {
         timestamp: timestamp,
         artist: song.artist,
         track: song.track,
-        album: song.album
+        album: song.album || '_'
       })
   sendPost(post_data, callback)
 }
